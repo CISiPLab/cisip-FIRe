6867bee1571a4de7cd9dd333b516df5da93b732e
Hoe Jiun Tian <jiuntian@gmail.com>
2021-12-24 20:51:18 +0800

    fix mistake in argparse

--- inference.py
+++ inference.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     parser.add_argument('--k', help="retrieve first top k", type=int, default=10)
     parser.add_argument('--ip', help='ip address', type=str, default='0.0.0.0')
     parser.add_argument('--port', help='tcp port', type=int, default=8000)
-    parser.add_argument('--log', help='enable log', type=bool, action='store_true')
+    parser.add_argument('--log', help='enable log', action='store_true')
     args = parser.parse_args()
 
     if args.log:
